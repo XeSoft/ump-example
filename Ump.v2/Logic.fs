8af2ee79db6a374c4fbe06c8d32b3d53ff91f65e
Kasey Speakman <kasey@cornerspeed.com>
2023-08-03 17:22:29 -0500

    added version with cleaner separation of logic and environment

---
+++ Ump.v2/Logic.fs
@@ -0,0 +1,24 @@
+﻿namespace Ump
+
+type Logic<'initArg, 'model, 'effect, 'msg> =
+    {
+        init: 'initArg -> 'model * 'msg
+        update: 'model -> 'msg -> 'model * 'effect list
+    }
+
+
+module Logic =
+
+    /// Creates a test function from init and update.
+    /// The returned function takes initArg and msgs and returns
+    /// a model and effect list that you can test against expected values.
+    let createTest logic =
+        let test initArg msgs =
+            let init = logic.init initArg
+            // ignore previous effects
+            let update (model, _) msg =
+                logic.update model msg
+            msgs
+            |> List.fold update init
+        test
+
---
+++ Ump.v2/Ump.Task.fs
@@ -0,0 +1,53 @@
+﻿namespace Ump.Task
+
+open System.Threading.Tasks
+open Ump
+
+type Ump<'initArg, 'model, 'effect, 'msg> =
+    {
+        /// perform is assumed to have it's dependencies partially applied
+        perform: 'effect -> Task<'msg>
+        logic: Logic<'initArg, 'model, 'effect, 'msg>
+    }
+
+
+module Ump =
+
+    /// perform is assumed to have it's dependencies partially applied
+    let create
+        (perform: 'effect -> Task<'msg>)
+        (logic: Logic<'initArg, 'model, 'effect, 'msg>)
+        : Ump<'initArg, 'model, 'effect, 'msg> =
+        {
+            perform = perform
+            logic = logic
+        }
+
+    let resume
+        (prog: Ump<'initArg, 'model, 'effect, 'msg>)
+        (model: 'model)
+        (msg: 'msg)
+        : Task<'model> =
+        task {
+            let mutable model = model
+            let msgs = System.Collections.Generic.Queue<'msg>(1)
+            msgs.Enqueue(msg)
+            while msgs.Count > 0 do
+                let msg = msgs.Dequeue()
+                let model_, effects = prog.logic.update model msg
+                model <- model_
+                for effect in effects do
+                    let! newMsg = prog.perform effect
+                    msgs.Enqueue(newMsg)
+            return model
+        }
+
+    let run
+        (prog: Ump<'initArg, 'model, 'effect, 'msg>)
+        (initArg: 'initArg)
+        : Task<'model> =
+        task {
+            let model_, msg_ = prog.logic.init initArg
+            return! resume prog model_ msg_
+        }
+
